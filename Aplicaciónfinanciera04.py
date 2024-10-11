efc7cad683d80dcc39090b703215a21e4d2a4ddb
fer-vasco <fernandovasconi@hotmail.com>
2024-10-11 18:37:16 -0300

    Add files via upload

---
+++ Aplicaciónfinanciera04.py
@@ -0,0 +1,77 @@
+import requests
+import pandas as pd
+import streamlit as st
+from datetime import datetime
+import yfinance as yf
+
+
+def Parametros_de_visualizacion():
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 1000)
+
+
+def market_cap(ticker, api_key):
+    url = f'https://financialmodelingprep.com/api/v3/market-capitalization/{ticker}?apikey={api_key}'
+    response = requests.get(url)
+    resultado = response.json()
+    
+    return resultado[0]['marketCap']
+
+
+def y_market_cap(ticker):
+    info = yf.Ticker(ticker).fast_info
+
+    return info['market_cap']
+
+
+def fetch_gainers(api_key):
+    url = f'https://financialmodelingprep.com/api/v3/stock_market/gainers?apikey={api_key}'
+    response = requests.get(url)
+    gainers = response.json()
+
+    
+    df_empresas = pd.DataFrame()
+    simbolos = []
+    nombres = []
+    cambios = []
+    capitales = []
+
+    for gainer in gainers:
+
+        simbolos.append(gainer['symbol'])
+        nombres.append(gainer['name'])
+        cambios.append(gainer['changesPercentage'])
+        capitales.append(y_market_cap(gainer['symbol']))
+
+        df_empresas = pd.DataFrame(list(zip(simbolos, nombres, cambios, capitales)),
+               columns =['Ticker', 'Empresa', 'Cambio', 'Capital'])
+
+        
+    df_empresas.sort_values(by=['Capital'], ascending=False, inplace=True)
+    df_empresas.set_index('Ticker', inplace=True)
+    
+    return df_empresas
+
+
+def Calcular_e_imprimir_df(api_key):
+    resultados = fetch_gainers(api_key)
+    st.dataframe(resultados)
+    
+
+
+
+# Inicio del programa
+api_key = 'BKewxsq6oAF5okFIZ5b84WGWGiy3kiOm'
+clave = str(123)
+st.title('🍁 Resultados!')
+text_input = st.text_input("Clave 👇", type="password")
+
+if text_input:
+    if str(text_input) == clave:
+        Parametros_de_visualizacion()
+        corrida = datetime.now()
+        corrida = corrida.strftime('%m-%d-%y %H:%M')
+        st.write(f'Datos actualizados el {corrida}')
+        Calcular_e_imprimir_df(api_key)
+    else:
+        st.write("Clave incorrecta")
