2679d894b5115a90f8951eddd8641e61dee861eb
fer-vasco <fernandovasconi@hotmail.com>
2024-10-11 22:35:57 -0300

    Update Aplicaciónfinanciera04.py

--- Aplicaciónfinanciera04.py
+++ Aplicaciónfinanciera04.py
@@ -71,7 +71,7 @@ def Calcular_e_imprimir_df(api_key):
 # Inicio del programa
 api_key = 'BKewxsq6oAF5okFIZ5b84WGWGiy3kiOm'
 clave = str(123)
-st.title('🍁 Resultados! (y)')
+st.title('🍁 Resultados')
 text_input = st.text_input("Clave 👇", type="password")
 
 if text_input:
